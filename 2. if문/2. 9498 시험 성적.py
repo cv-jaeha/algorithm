513b0c931f3ea6573ae27bd01ca9af0719608531
taki0412 <taki041210@naver.com>
2022-02-28 22:22:19 +0900

    [9498] 시험 성적

---
+++ 2. if문/2. 9498 시험 성적.py
@@ -0,0 +1,11 @@
+i = int(input())
+if 100 >= i >= 90:
+    print("A")
+elif 90 > i >= 80:
+    print("B")
+elif 80 > i >= 70:
+    print("C")
+elif 70 > i >= 60:
+    print("D")
+else:
+    print("F")
