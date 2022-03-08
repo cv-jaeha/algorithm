54a488fb41d7e5bbae11dc3f17d9e68d489339cc
taki0412 <taki041210@naver.com>
2022-03-08 19:14:35 +0900

    [10871] X보다 작은 수

---
+++ 3. for문/11. 10871 X보다 작은 수.py
@@ -0,0 +1,6 @@
+a, b = map(int, input().split())
+list = list(map(int, input().split()))
+for i in list:
+    if i < b:
+        print(i, end=' ')
+# Reference : https://velog.io/@jsw8050/%EB%B0%B1%EC%A4%80-for%EB%AC%B8-10871%EB%B2%88-X%EB%B3%B4%EB%8B%A4-%EC%9E%91%EC%9D%80-%EC%88%98-Python
