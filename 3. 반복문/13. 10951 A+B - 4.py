1bfaf7545581b5078c1b3dbc7039f5ee8d590b22
taki0412 <taki041210@naver.com>
2022-03-27 23:17:17 +0900

    [10951] A+B - 4

---
+++ 3. 반복문/13. 10951 A+B - 4.py
@@ -0,0 +1,8 @@
+while True:
+    try:
+        a, b = map(int, input().split())
+        print(a+b)
+    except:
+        break
+
+# Reference : https://velog.io/@jsw8050/백준-while문-10951번-AB-4-Python
